3abd2e944ae75be9e9ee301157086465bb24439b
lexprom <lexprom009@gmail.com>
2019-03-14 22:05:12 +0400

    Added the 3 task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 import Biomass from "./CalculatingBiomass";
+import ExpectedValueDiscrete from './ExpectedValueDiscrete';
+
+import "./styles.css"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Biomass />
+        <ExpectedValueDiscrete />
       </div>
     );
   }
---
+++ src/ExpectedValueDiscrete/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+import Image from "./assets/task3.png"
+
+const NUMBER_OF_EXPERIMENTS = 1000;
+const MIN_NUMBER_OF_DETAILS = 5;
+
+class ExpectedValueDiscrete extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			result: 0
+		}
+	}
+
+	calculateExpectedValues = () => {
+		const details = [0, 0, 1, 1, 1, 2, 2, 2, 2, 3]; // odds of details
+		const resultsExpectedValues = [];
+		for (let index = 0; index < NUMBER_OF_EXPERIMENTS; index++) {
+			let temp = 0;
+			let count = 0;
+			while (temp <= MIN_NUMBER_OF_DETAILS) {
+				temp += details[Math.floor(Math.random() * details.length)];
+				count++;
+			}
+			resultsExpectedValues.push(count);
+		}
+		const sumExpctedValues = resultsExpectedValues.reduce((acc, currentVal) => acc + currentVal);
+		this.setState({ result: sumExpctedValues / NUMBER_OF_EXPERIMENTS });
+	}
+
+	componentDidMount() {
+		this.calculateExpectedValues();
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Task 3</h1>
+				<img src={Image} height="150px" width="550px" alt="" />
+				<div>Result:{this.state.result} </div>
+				<button onClick={this.calculateExpectedValues}>Calculate</button>
+			</div>
+		)
+	}
+}
+
+export default ExpectedValueDiscrete;
